8d4c6cbaab62f149b07da659376f6613fd0e750a
kino1134 <inoue1134@gmail.com>
2018-08-26 05:26:51 +0900

    認証側のエントリポイントを追加

---
+++ client/src/Authenticate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Authenticate extends Component {
+  render() {
+    return (
+      <div className="Authenticate">
+        Auth
+      </div>
+    );
+  }
+}
+
+export default Authenticate
+
