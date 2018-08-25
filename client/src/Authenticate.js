a0b065b1bca8ff1bd96a7fb047d40e0a151027e8
kino1134 <inoue1134@gmail.com>
2018-08-26 06:38:26 +0900

    React Test

--- client/src/Authenticate.js
+++ client/src/Authenticate.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
 class Authenticate extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      t: "",
+      m: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch(process.env.REACT_APP_API_ROOT + '/hellos/5b77fe400162f504e541081b').then(res => {
+      res.json().then(json => {
+        console.log(json)
+        this.setState({
+          t: json.id,
+          m: json.message
+        })
+      })
+    })
+  }
+
   render() {
     return (
       <div className="Authenticate">
+        <div>
+          {this.state.t} - {this.state.m}
+        </div>
         <div>
           {process.env.REACT_APP_API_ROOT}
         </div>
