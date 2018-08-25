5730d2e7127b4621692785f85f7b3ddc89616d34
kino1134 <inoue1134@gmail.com>
2018-08-26 05:53:50 +0900

    クライアント側での環境設定の読み込みテスト

--- client/src/Authenticate.js
+++ client/src/Authenticate.js
@@ -4,7 +4,12 @@ class Authenticate extends Component {
   render() {
     return (
       <div className="Authenticate">
-        Auth
+        <div>
+          {process.env.REACT_APP_API_ROOT}
+        </div>
+        <div>
+          Auth
+        </div>
       </div>
     );
   }
