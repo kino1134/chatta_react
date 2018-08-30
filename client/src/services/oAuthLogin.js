bc88c82b19ac087cfc18420ffabcd345eff90015
kino1134 <inoue1134@gmail.com>
2018-08-31 01:34:27 +0900

    OAuth認証の共通処理を一つにまとめる

---
+++ client/src/services/oAuthLogin.js
@@ -0,0 +1,44 @@
+export const openPopup = (url) => {
+  const width = 500
+  const height = 600
+  const left = (window.screen.width - width) / 2
+  const top = (window.screen.height - height) / 2
+
+  const popup = window.open(url, 'oauth_login',
+    `width=${width},height=${height},left=${left},top=${top}`)
+
+  return popup
+}
+
+export const listenPopup = async (popup, resolve, reject) => {
+  if (!resolve) {
+    // はじめにPromiseでくるむ
+    return new Promise((resolve, reject) => {
+      listenPopup(popup, resolve, reject)
+    })
+  } else {
+    // 認証情報の取得
+    // same origin policyの回避のため、エラーは握りつぶす
+    let token, user
+    try {
+      // TODO: 取得内容・方法は要検討
+      const form = popup.document.forms.auth
+      token = form.token.value
+      user = form.user.value
+    } catch (err) {}
+
+    if (token) {
+      // 取得できた場合
+      popup.close()
+      resolve({ token, user })
+    } else if (popup.closed) {
+      // 取得前にポップアップを閉じてしまった場合
+      reject({ errors: "Authenticated was cancelled." })
+    } else {
+      // それ以外の場合は待つ
+      setTimeout(() => {
+        listenPopup(popup, resolve, reject)
+      }, 0)
+    }
+  }
+}
