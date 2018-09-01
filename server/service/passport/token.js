28d09926e66802b99b911f807cd79442972cd17c
kino1134 <inoue1134@gmail.com>
2018-09-01 19:16:56 +0900

    トークンの認証方式を整備する

--- server/service/passport/token.js
+++ server/service/passport/token.js
@@ -1,6 +1,8 @@
 import passport from 'passport'
+import http from 'http'
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
 import config from '../../config'
+import User from '../../model/User'
 
 // Authenticate by Token
 const jwtConfig = {
@@ -11,15 +13,32 @@ const jwtConfig = {
     ExtractJwt.fromAuthHeaderAsBearerToken()
   ])
 }
-const jwtAuth = (payload, done) => {
-  // TODO: Find User
-  console.log(payload)
-  return done(null, payload)
+const jwtAuth = ({ id }, done) => {
+  console.log('id' + id)
+  return User.findById(id)
+    .then(user => done(null, user))
+    .catch(err => done(err, null))
 }
 passport.use(new JwtStrategy(jwtConfig, jwtAuth))
-export const token = (opts) => {
-  const option = {
-    session: false
+export const token = ({ required = true, roles = User.roles } = {}) => {
+  return (req, res, next) => {
+    const auth = passport.authenticate('jwt', { session: false }, (err, user, info) => {
+      // ユーザ情報の取得に失敗、または権限がなかった場合
+      if (err || (required && !user) || (required && !~roles.indexOf(user.role))) {
+        res.status(401).end(http.STATUS_CODES[401])
+        return null
+      }
+
+      // passportのログイン処理を呼び出す
+      req.logIn(user, { session: false }, (err) => {
+        if (err) {
+          res.status(401).end(http.STATUS_CODES[401])
+          return null
+        }
+        next()
+      })
+    })
+
+    return auth(req, res, next)
   }
-  return passport.authenticate('jwt', Object.assign(option, opts))
 }
