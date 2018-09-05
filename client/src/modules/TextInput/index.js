21f0ca9b912532935a77a0f608c7a86bd3dc8398
kino1134 <inoue1134@gmail.com>
2018-09-06 00:16:15 +0900

    テキストフィールドをコンポーネント化する

---
+++ client/src/modules/TextInput/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+import InputMessage from '../InputMessage'
+
+class TextInput extends Component {
+
+  isError (errors, name) {
+    if (!errors) return ''
+    return errors.find(e => e.param === name) ? 'is-danger' : ''
+  }
+
+  render () {
+    const {
+      children,
+      className,
+      name,
+      color,
+      errors,
+      ...rest
+    } = this.props
+
+    // 最小限必要なクラス名を追加する
+    const classes = [
+      'input',
+      this.isError(errors, name),
+      className
+    ].join(' ')
+
+    // 必須アイコンを付与する
+    let requireIcon = null
+    if (this.props.required) {
+      requireIcon = (
+        <span className="icon is-right has-text-danger">
+          <i className="fas fa-asterisk fa-xs"></i>
+        </span>
+      )
+    }
+
+    return (
+      <div className="field">
+        <div className={'control' + (this.props.required ?  ' has-icons-right' : '')}>
+          <input type="text" name={name} className={classes} {...rest} />
+          {requireIcon}
+        </div>
+        <InputMessage name={name} color={color || 'danger'} errors={errors} />
+        {children}
+      </div>
+    )
+  }
+}
+
+export default TextInput
