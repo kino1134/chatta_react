d9e52785913b7d28b2dc1bb70152a4d88fb8765d
kino1134 <inoue1134@gmail.com>
2018-09-08 01:00:31 +0900

    GETメソッドを叩くfetchを追加

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,5 +2,6 @@ const apiCall = async ({ url, method, body }) =>
   fetch(url, { method, body, headers: { 'content-type': 'application/json' } })
 
 export default {
-  post: async (url, body) => apiCall({ method: 'POST', url, body })
+  post: async (url, body) => apiCall({ method: 'POST', url, body }),
+  get: async (url) => apiCall({ method: 'GET', url })
 }
