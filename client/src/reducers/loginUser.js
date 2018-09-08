e8c0438d8ec4dd842ffc33d590879227f0268f45
kino1134 <inoue1134@gmail.com>
2018-09-08 19:28:55 +0900

    storeへの保管方法を簡素化

--- client/src/reducers/loginUser.js
+++ client/src/reducers/loginUser.js
@@ -3,7 +3,7 @@ import { SET_LGOIN_USER } from '../actions/loginUser'
 export default (state=null, action) => {
   switch (action.type) {
     case SET_LGOIN_USER:
-      return { ...state, user: action.payload }
+      return action.payload
     default:
       return state
   }
