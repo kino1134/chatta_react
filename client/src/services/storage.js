8108791c3bc0b5f00c1872056c8a1583e72fea5d
kino1134 <inoue1134@gmail.com>
2018-09-08 14:08:38 +0900

    ログイン状態を判断するメソッドを追加

--- client/src/services/storage.js
+++ client/src/services/storage.js
@@ -1,3 +1,5 @@
+import decode from 'jwt-decode'
+
 const ACCESS_TOKEN = 'access_token'
 
 export const setAccessToken = (token) =>
@@ -8,3 +10,22 @@ export const getAccessToken = () =>
 
 export const clearAccessToken = () =>
   localStorage.removeItem(ACCESS_TOKEN)
+
+export const loggedIn = () => {
+  const token = getAccessToken()
+  return !!token && !isTokenExpired(token)
+}
+
+const getTokenExpirationDate = (token) => {
+  const claim = decode(token)
+  if (!claim.exp) return null
+
+  const date = new Date(0)
+  date.setUTCSeconds(claim.exp)
+  return date
+}
+
+const isTokenExpired = (token) => {
+  const expirationDate = getTokenExpirationDate(token)
+  return expirationDate < new Date()
+}
