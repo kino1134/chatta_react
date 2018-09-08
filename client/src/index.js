9069ba9a4016df92f01de5ddf4adf4a0aae44ad9
kino1134 <inoue1134@gmail.com>
2018-09-08 16:28:16 +0900

    ReactにStoreを混ぜ込む

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 import LoginCheck from './modules/LoginCheck'
 import App from './modules/App'
+import reducer from './reducers'
+
+const store = createStore(reducer)
 
 ReactDOM.render((
   <BrowserRouter>
-    <LoginCheck>
-      <Switch>
-        <Route exact path="/"component={ App } />
-        <Route component={ App } />
-      </Switch>
-    </LoginCheck>
+    <Provider store={store}>
+      <LoginCheck>
+        <Switch>
+          <Route exact path="/"component={ App } />
+          <Route component={ App } />
+        </Switch>
+      </LoginCheck>
+    </Provider>
   </BrowserRouter>
 ), document.getElementById('root'))
 registerServiceWorker()
