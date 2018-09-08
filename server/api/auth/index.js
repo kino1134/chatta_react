ee01d18c77ca3c43b280ec4be2c4317ed5341fe7
kino1134 <inoue1134@gmail.com>
2018-09-08 14:10:49 +0900

    トークンをリフレッシュするAPIを追加

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { login } from './controller'
 import validator from './validator'
-import { google, github, password } from '../../service/passport'
+import { google, github, password, token } from '../../service/passport'
 import { validate } from '../../service/response'
 
 const router = new Router()
@@ -9,5 +9,6 @@ const router = new Router()
 router.get('/google', google(), login)
 router.get('/github', github(), login)
 router.post('/password', validate(validator, 'ログインに失敗しました。'), password(), login)
+router.post('/refresh', token(), login)
 
 export default router
