61509106e9a9d2e3c079a2fe509f8061c1c5187e
kino1134 <inoue1134@gmail.com>
2018-09-16 20:39:44 +0900

    ユーザ情報を更新するreducerを追加

--- client/src/actions/loginUser.js
+++ client/src/actions/loginUser.js
@@ -1,3 +1,5 @@
 export const SET_LGOIN_USER = 'chatta_react/set_login_user'
+export const UPDATE_LOGIN_USER = 'chatta_react/update_login_user'
 
 export const setLoginUser = (user) => ({ type: SET_LGOIN_USER, payload: user })
+export const updateLoginUser = (user) => ({ type: UPDATE_LOGIN_USER, payload: user })
--- client/src/reducers/loginUser.js
+++ client/src/reducers/loginUser.js
@@ -1,9 +1,11 @@
-import { SET_LGOIN_USER } from '../actions/loginUser'
+import { SET_LGOIN_USER, UPDATE_LOGIN_USER } from '../actions/loginUser'
 
 export default (state=null, action) => {
   switch (action.type) {
     case SET_LGOIN_USER:
       return action.payload
+    case UPDATE_LOGIN_USER:
+      return {...state, ...action.payload}
     default:
       return state
   }
