ce4d3463cd18054e8b3e9772a36ff289ef92d971
kino1134 <inoue1134@gmail.com>
2018-09-16 15:35:25 +0900

    パスワード初期化を行うAPIを追加

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import { token } from '../../service/passport'
 import { validate } from '../../service/response'
 
-import { showMe, create, updatePassword } from './controller'
-import validator, { updatePasswordValidator } from './validator'
+import { showMe, create, updatePassword, initPassword } from './controller'
+import validator, { updatePasswordValidator, initPasswordValidator } from './validator'
 
 const router = new Router()
 
 router.get('/me', token(), showMe)
 router.post('/', validate(validator, '入力内容が間違っています'), create)
 router.put('/password', token(), validate(updatePasswordValidator, '入力内容が間違っています'), updatePassword)
+router.put('/password/init', validate(initPasswordValidator, '入力内容が間違っています'), initPassword)
 
 export default router
