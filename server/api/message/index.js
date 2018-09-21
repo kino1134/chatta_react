e05d510b545bcac191e026538ee56bb17b6fdbca
kino1134 <inoue1134@gmail.com>
2018-09-22 02:47:24 +0900

    ページング機能を持ったメッセージ取得APIを追加

--- server/api/message/index.js
+++ server/api/message/index.js
@@ -2,11 +2,12 @@ import { Router } from 'express'
 import { token } from '../../service/passport'
 import { validate } from '../../service/response'
 
-import { post, getOne, getAll } from './controller'
-import validator from './validator'
+import { post, get, getOne, getAll } from './controller'
+import validator, { getValidator } from './validator'
 
 const router = new Router()
 
+router.get('/', validate(getValidator, 'パラメータが間違っています'), token(), get)
 router.get('/one', token(), getOne)
 router.get('/all', token(), getAll)
 router.post('/', validate(validator, 'パラメータが間違っています'), token(), post)
--- server/api/message/validator.js
+++ server/api/message/validator.js
@@ -3,4 +3,8 @@ import { check } from 'express-validator/check'
 const content = check('content').trim()
   .not().isEmpty().withMessage('必須です。')
 
+const id = check('last').trim()
+  .optional().isMongoId().withMessage('形式が正しくありません')
+
+export const getValidator = [ id ]
 export default [ content ]
