e0fd7999941a08e081a26cb1791b742ea791cb29
kino1134 <inoue1134@gmail.com>
2018-10-07 13:25:39 +0900

    ファイルアップロード用のモーダルを分離する

--- client/src/modules/RoomPost/index.js
+++ client/src/modules/RoomPost/index.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react'
-import Modal from 'react-modal'
 import './index.css'
 
 import EmojiPicker from '../EmojiPicker'
+import RoomPostUploadModal from '../RoomPostUploadModal'
 
 import apiHandler from '../../helpers/apiHandler'
 
 import api from '../../services/api'
 import socket from '../../services/socket'
-
-Modal.setAppElement('#root')
-const fileUploadStyles = {
-  content: {
-    minWidth: '70%',
-    maxWidth: 'calc(100% - 32px)',
-    maxHeight: '640px',
-    top: 'auto',
-    left: 'auto',
-    right: 'auto',
-    bottom: 'auto',
-  },
-  overlay: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,.4)'
-  }
-}
 
 class RoomPost extends Component {
 
@@ -34,6 +15,8 @@ class RoomPost extends Component {
     super(props)
 
     this.changeHandler = this.changeHandler.bind(this)
+    this.clearSelectFile = this.clearSelectFile.bind(this)
+    this.updateTyping = this.updateTyping.bind(this)
 
     this.typing = false
     this.lastTypingTime = null
@@ -44,8 +27,6 @@ class RoomPost extends Component {
       openEmojiPicker: null,
       text: "",
       selectFile: "",
-      uploadText: "",
-      uploading: false,
       typeUser: ""
     }
 
@@ -140,29 +121,13 @@ class RoomPost extends Component {
 
   selectFile (e) {
     this.fileInput.current.click()
-    this.setState({ uploadText: this.state.text })
   }
 
-  clearSelectFile (e) {
-    if (this.state.uploading) return
-    this.setState({ selectFile: '' })
-  }
-
-  fileUpload (e) {
-    if (this.state.uploading) return
-
-    this.setState({ uploading: true })
-    const body = new FormData()
-    body.append('attachFile', this.fileInput.current.files[0])
-    body.append('content', this.state.uploadText)
-    api.postData({ url: '/api/messages/upload', body }).catch(err => { console.log(err) })
-      .then(() => this.setState({ text: '', selectFile: '', uploading: false }) )
-  }
-
-  uploadEnter (e) {
-    if (e.key === 'Enter' && e.ctrlKey) {
-      e.preventDefault()
-      this.fileUpload(e)
+  clearSelectFile (e, isAll) {
+    if (isAll) {
+      this.setState({ selectFile: '', text: '' })
+    } else {
+      this.setState({ selectFile: '' })
     }
   }
 
@@ -207,39 +172,9 @@ class RoomPost extends Component {
           </p>
         </div>
         <EmojiPicker position={this.state.openEmojiPicker} onRequestClose={e => this.setState({ openEmojiPicker: null }) }
-         selectEmoji={k => this.setState({ text: `${this.state.text} ${k} ` }) } />
-        <Modal contentLabel="ファイルアップロード" style={fileUploadStyles}
-          isOpen={!!this.state.selectFile} onRequestClose={e => this.clearSelectFile(e) }>
-          <div className="content delete-message-confirm">
-            <h3 className="header">
-              ファイルアップロード
-              <a onClick={e => this.clearSelectFile(e) }><i className="fas fa-times"></i></a>
-            </h3>
-            <p>このファイルをアップロードしますか？</p>
-            <div className="content message-container">
-              {this.state.selectFile.replace('C:\\fakepath\\', '')}
-            </div>
-            <div className="field">
-              <textarea name="uploadText" className="textarea" placeholder="メッセージの追加" autoFocus
-                rows={this.rows(this.state.uploadText)} value={this.state.uploadText} onChange={this.changeHandler}
-                onInput={(e) => this.updateTyping(e)} onKeyDown={(e) => this.uploadEnter(e)}>
-              </textarea>
-            </div>
-            <nav className="level is-mobile">
-              <div className="level-left">
-                <div className="level-item">
-                  <a className="button" onClick={e => this.clearSelectFile(e) }>キャンセル</a>
-                </div>
-                <div className="level-item">
-                  <a className={`button is-success ${this.state.uploading ? 'is-loading' : ''}`}
-                    onClick={e => this.fileUpload(e)}>
-                    アップロード
-                  </a>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </Modal>
+          selectEmoji={k => this.setState({ text: `${this.state.text} ${k} ` }) } />
+        <RoomPostUploadModal uploadText={this.state.text} fileInput={this.fileInput} selectFile={this.state.selectFile}
+          rows={this.rows} updateTyping={this.updateTyping} clearSelectFile={this.clearSelectFile} />
       </footer>
     )
   }
---
+++ client/src/modules/RoomPostUploadModal/index.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import Modal from 'react-modal'
+
+import api from '../../services/api'
+
+Modal.setAppElement('#root')
+const fileUploadStyles = {
+  content: {
+    minWidth: '70%',
+    maxWidth: 'calc(100% - 32px)',
+    maxHeight: '640px',
+    top: 'auto',
+    left: 'auto',
+    right: 'auto',
+    bottom: 'auto',
+  },
+  overlay: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,.4)'
+  }
+}
+
+class RoomPostUploadModal extends Component {
+  constructor (props) {
+    super(props)
+
+    this.changeHandler = this.changeHandler.bind(this)
+
+    this.state = {
+      uploading: false,
+      uploadText: ''
+    }
+  }
+
+  changeHandler (e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  fileUpload (e) {
+    if (this.state.uploading) return
+
+    this.setState({ uploading: true })
+    const body = new FormData()
+    body.append('attachFile', this.props.fileInput.current.files[0])
+    body.append('content', this.state.uploadText)
+    api.postData({ url: '/api/messages/upload', body }).catch(err => { console.log(err) })
+      .then(() => {
+        this.props.clearSelectFile(e, true)
+        this.setState({ uploading: false })
+      })
+  }
+
+  uploadEnter (e) {
+    if (e.key === 'Enter' && e.ctrlKey) {
+      e.preventDefault()
+      this.fileUpload(e)
+    }
+  }
+
+  render () {
+    return (
+      <Modal contentLabel="ファイルアップロード" style={fileUploadStyles} isOpen={!!this.props.selectFile}
+        onAfterOpen={e => this.setState({ uploadText: this.props.uploadText })} onRequestClose={e => this.props.clearSelectFile(e) }>
+        <div className="content delete-message-confirm">
+          <h3 className="header">
+            ファイルアップロード
+            <a onClick={e => this.props.clearSelectFile(e) }><i className="fas fa-times"></i></a>
+          </h3>
+          <p>このファイルをアップロードしますか？</p>
+          <div className="content message-container">
+            {this.props.selectFile.replace('C:\\fakepath\\', '')}
+          </div>
+          <div className="field">
+            <textarea name="uploadText" className="textarea" placeholder="メッセージの追加" autoFocus
+              rows={this.props.rows(this.state.uploadText)} value={this.state.uploadText} onChange={this.changeHandler}
+              onInput={(e) => this.props.updateTyping(e)} onKeyDown={(e) => this.uploadEnter(e)}>
+            </textarea>
+          </div>
+          <nav className="level is-mobile">
+            <div className="level-left">
+              <div className="level-item">
+                <a className="button" onClick={e => this.props.clearSelectFile(e, false) }>キャンセル</a>
+              </div>
+              <div className="level-item">
+                <a className={`button is-success ${this.state.uploading ? 'is-loading' : ''}`}
+                  onClick={e => this.fileUpload(e)}>
+                  アップロード
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </Modal>
+    )
+  }
+}
+
+export default RoomPostUploadModal
